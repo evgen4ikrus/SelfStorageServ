b65f77b7c8a2b91bb6be679aa03cc59eda5d673f
tbaiguzhinov <tim.baiguzhinov@gmail.com>
2022-06-30 17:40:54 +0600

    Made new version of DB structure

--- storage_bot/bot/models.py
+++ storage_bot/bot/models.py
@@ -1,18 +1,82 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
-from django.db.models.fields import DateTimeField
-from django.db.models.fields.related import ForeignKey
+
 
 class User(models.Model):
-    name = models.CharField(
-        verbose_name='Имя и Фамилия пользователя',
-        max_length=50
+    """Модель пользователя."""
+
+    name = models.CharField('Имя и Фамилия пользователя', max_length=50)
+    email = models.CharField(
+        'Адрес электронной почты',
+        max_length=50,
+        blank=True
     )
-    email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Storage(models.Model):
+    """Модель склада."""
+
+    address = models.CharField('Адрес', max_length=200)
+    lat = models.FloatField('Широта')
+    lon = models.FloatField('Долгота')
+
+    def __str__(self):
+        return "Склад по адресу: {self.address}"
+
 
 class Box(models.Model):
-    adress = models.CharField(max_length=200)
-    number = models.IntegerField()
-    user = ForeignKey(User, on_delete=models.CASCADE)
-    period = DateTimeField()
+    """Модель ячейки."""
 
+    status_choices = [
+        (0, "Free"),
+        (1, "Booked"),
+        (2, "Paid"),
+        (3, "Occupied"),
+    ]
+
+    renter = models.ForeignKey(
+        User,
+        on_delete=models.DO_NOTHING,
+        verbose_name='Арендатор',
+        related_name='boxes',
+        blank=True,
+    )
+    rent_period = models.DateTimeField('Дата окончания аренды', blank=True)
+    storage = models.ForeignKey(
+        Storage,
+        on_delete=models.CASCADE,
+        verbose_name='Склад',
+        related_name='boxes',
+    )
+    status = models.CharField(
+        max_length=2,
+        choices=status_choices,
+        default=status_choices[0]
+    )
+
+    def __str__(self):
+        return 'Коробка {self.id}, склад {self.storage.id}'
+
+    class Meta:
+        verbose_name_plural = "Boxes"
+
+
+class Order(models.Model):
+    """Модель заказа."""
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Заказчик',
+        related_name='orders'
+    )
+    creation_date = models.DateTimeField('Дата создания заказа')
+    box = models.ForeignKey(
+        Box,
+        on_delete=models.CASCADE,
+        verbose_name='Номер ячейки',
+        related_name='orders'
+    )
+    comment = models.CharField(blank=True, max_length=100)
