bbe2ec92542f91296fa9227de49acfa8b42d8011
Victor Abdukarimov <viredmi@gmail.com>
2022-06-30 09:45:40 +0300

    Fixed imports

--- storage_bot/bot/management/commands/bot.py
+++ storage_bot/bot/management/commands/bot.py
@@ -2,7 +2,7 @@ import os
 
 from bot.models import User
 
-from dotenv import load_dotenv()
+from dotenv import load_dotenv
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
@@ -27,9 +27,19 @@ class Command(BaseCommand):
             await update.message.reply_text(
                 f'Hello {update.effective_user.first_name}'
             )
+        async def start(
+            update: Update,
+            context: ContextTypes.DEFAULT_TYPE
+        ):
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="Hello!"
+            )
 
         app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
+        
+        start_handler = CommandHandler('start', start)
 
         app.add_handler(CommandHandler("hello", hello))
-
+        app.add_handler(start_handler)
         app.run_polling()
