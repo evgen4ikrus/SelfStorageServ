feb469dd5f4132cb524fb69ee3647546b6117ed8
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-06-30 12:21:54 +0300

    Создал модели (черновой вариант)

--- storage_bot/bot/models.py
+++ storage_bot/bot/models.py
@@ -1,18 +1,64 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
-from django.db.models.fields import DateTimeField
-from django.db.models.fields.related import ForeignKey
+
 
 class User(models.Model):
     name = models.CharField(
-        verbose_name='Имя и Фамилия пользователя',
+        verbose_name='Имя',
         max_length=50
     )
-    email = models.CharField(max_length=50)
+    surname = models.CharField(
+        verbose_name='Фамилия',
+        max_length=50
+    )
+    email = models.EmailField(
+        verbose_name='Email',
+    )
+    adress = models.CharField(
+        max_length=100,
+        verbose_name='Адрес',
+    )
+    # phone = models.
+    
 
-class Box(models.Model):
-    adress = models.CharField(max_length=200)
-    number = models.IntegerField()
-    user = ForeignKey(User, on_delete=models.CASCADE)
-    period = DateTimeField()
+class Stock:
+    adress = models.CharField(
+        max_length=100,
+        verbose_name='Адрес',
+    )
+    # working_mode = 
+    
+    
+class Storage_cell(models.Model):
+    stock = models.ForeignKey(
+        Stock,
+        verbose_name='Склад',
+        on_delete=models.CASCADE
+    )
+    size = models.FloatField(
+        verbose_name='Размер',
+    )
+    price = models.FloatField(
+        verbose_name='Цена аренды',
+    )
+    booked = models.BooleanField(
+        verbose_name='Забронировано',
+    )
+
+
+class Order(models.Model):
+    create_date = models.DateField(
+        verbose_name='Дата создания заказа'
+    )
+    # rental_period = 
+    user = models.ForeignKey(
+        User,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE
+    )
+    storage_cell = models.ForeignKey(
+        Storage_cell,
+        verbose_name='Ячейка',
+        on_delete=models.CASCADE
+    )
+
 
