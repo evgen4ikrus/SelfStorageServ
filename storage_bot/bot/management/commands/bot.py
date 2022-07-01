faa59abb9695b285e69297e7f86a76922a13e1c5
Victor Abdukarimov <viredmi@gmail.com>
2022-07-01 12:43:12 +0300

    Added base autorization func

--- storage_bot/bot/management/commands/bot.py
+++ storage_bot/bot/management/commands/bot.py
@@ -5,11 +5,39 @@ from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler, Updater
 
+from bot.models import User, Cell
+
+# Кладем telegram id при активации бота
+def add_tgid(telegram_id: int):
+    user = User(tgid=telegram_id)
+    user.save()
+
+# Проверяем есть ли пользователь в БД
+def find_user(telegram_id: int):
+    find_user = User.objects.all().filter(tgid=telegram_id)
+    if not find_user:
+        return False
+    return True
+
+
+def make_order(telegram_id: int):
+    pass
+
+def get_cells():
+    cells = Cell.objects.all()
+    return cells
+
+
 
 def start(update: Update, context: CallbackContext):
-    user = update.message.from_user
-    message = "Hello, {}!".format(user['username'])
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    user_id = update.message.from_user.id
+    if find_user(user_id):
+        message = f"Hello, your id is {user_id}!"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    else:
+        add_tgid(user_id)
+        message = f"Описание проекта, соглашение о ПД"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
 class Command(BaseCommand):
@@ -21,6 +49,7 @@ class Command(BaseCommand):
         dispatcher = updater.dispatcher
 
         start_handler = CommandHandler('start', start)
-        dispatcher.add_handler(start_handler)
+        cells_handler = CommandHandler('cells', cells)
+        dispatcher.add_handler(start_handler, get_cells)
         updater.start_polling()
         updater.idle()
