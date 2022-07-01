37315e533b5855f18d035caff14d062c4854f87e
tbaiguzhinov <tim.baiguzhinov@gmail.com>
2022-07-01 17:13:59 +0600

    Add phonefield and fix models

--- storage_bot/bot/models.py
+++ storage_bot/bot/models.py
@@ -1,70 +1,96 @@
 from django.db import models
 from django.db.models.fields import DateTimeField, TimeField
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(models.Model):
-    tgid = models.IntegerField()
+    """Модель пользователя."""
+
+    telegram_id = models.IntegerField(
+        "ID пользователя в Telegram",
+        null=True,
+    )
     name = models.CharField(
+        "Имя",
         max_length=50,
-        blank=True
+        blank=True,
     )
     surname = models.CharField(
+        "Фамилия",
         max_length=50,
-        blank=True
+        blank=True,
     )
     email = models.EmailField(
-        blank=True
+        "Электронная почта",
+        blank=True,
     )
-    adress = models.CharField(
+    address = models.CharField(
+        "Адрес",
         max_length=100,
-        blank=True
+        blank=True,
     )
-    phone = models.CharField(
+    phone = PhoneNumberField(
+        "Номер телефона",
         max_length=20,
-        blank=True
+        blank=True,
     )
 
     def __str__(self):
-        return self.name
-    
+        return f"{self.name} {self.surname}"
+
 
 class Storage(models.Model):
+    """Модель скалада."""
+
     address = models.CharField(
-        max_length=100
+        "Адрес",
+        max_length=100,
     )
     work_time = models.CharField(
-        max_length=50
+        "Режим работы",
+        max_length=50,
     )
 
     def __str__(self):
-        return f'склад по адресу: {self.address}'
-    
-    
+        return f'Склад по адресу: {self.address}'
+
+
 class Cell(models.Model):
+    """Модель ячейки."""
+
     storage = models.ForeignKey(
         Storage,
         verbose_name='Склад',
-        related_name='boxes',
+        related_name='cells',
         on_delete=models.CASCADE
     )
     size = models.CharField(
-        max_length=100
+        "Размер",
+        max_length=100,
     )
-    price = models.FloatField()
+    price = models.FloatField(
+        "Цена",
+    )
     booked = models.BooleanField(
+        "Занято",
         default=False
-        )
-    lease_time = DateTimeField()
+    )
+    lease_time = DateTimeField(
+        "Дата окончания аренды",
+        blank=True,
+    )
 
     def __str__(self):
-        return 'Коробка {self.id}, склад {self.storage.id}'
-
-    class Meta:
-        verbose_name_plural = 'Boxes'
+        return f'Ячейка №{self.id}, склад №{self.storage.id}'
 
 
 class Order(models.Model):
-    create_time = models.DateTimeField()
+    """Модель заказа."""
+
+    create_date = models.DateTimeField(
+        "Дата создания",
+        null=True,
+    )
     user = models.ForeignKey(
         User,
         verbose_name='Заказчик',
@@ -78,10 +104,14 @@ class Order(models.Model):
         on_delete=models.CASCADE
     )
     measurer = models.BooleanField(
+        "Нужен ли замерщик",
         default=False
     )
     comment = models.CharField(
+        "Комментарий",
         max_length=100,
         blank=True
     )
 
+    def __str__(self):
+        return f"Заказ от: {self.user} по: {self.cell}"
