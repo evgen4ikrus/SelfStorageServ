26467da7733fe63ab1f59b6035ab456c4a40ee8e
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-07-02 18:45:47 +0300

    Исправил опечатку

--- storage_bot/bot/management/commands/bot.py
+++ storage_bot/bot/management/commands/bot.py
@@ -77,7 +77,7 @@ def display_menu(update: Update, context: CallbackContext):
             [KeyboardButton(text="Личный кабинет"),],
             [KeyboardButton(text="Создать новый заказ"),],
             [KeyboardButton(text="Наши тарифы"),],
-            [KeyboardButton(text="наши адреса"),],
+            [KeyboardButton(text="Наши адреса"),],
         ],
         resize_keyboard=True,
     )
