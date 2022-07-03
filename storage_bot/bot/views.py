a1db3e934821207147441549627e3d45653d9bca
tbaiguzhinov <tim.baiguzhinov@gmail.com>
2022-07-04 03:20:00 +0600

    Сделал первый набросок личного кабинета владельца складов

--- storage_bot/bot/views.py
+++ storage_bot/bot/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from cloudipsp import Api, Checkout
 from django.shortcuts import redirect
+from bot.models import Order, User
 
 
 def pay_cell(request):
@@ -13,3 +14,19 @@ def pay_cell(request):
     }
     url = checkout.url(data).get('checkout_url')
     return redirect(url)
+
+
+def orders_view(request):
+    active_orders = Order.objects.all()
+    context = {
+        'active_orders': active_orders,
+    }
+    return render(request, 'active_orders.html', context)
+
+
+def user_view(request, userid):
+    user = User.objects.filter(id=userid).get()
+    context = {
+        'user': user,
+    }
+    return render(request, 'user_info.html', context)
