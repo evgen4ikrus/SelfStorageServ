603625840bfbf7064f8bb43b3ca6d6138612068e
Victor Abdukarimov <viredmi@gmail.com>
2022-07-05 13:56:39 +0300

    Fixed handlers

--- storage_bot/bot/management/commands/bot.py
+++ storage_bot/bot/management/commands/bot.py
@@ -3,11 +3,21 @@ import datetime
 
 from django.core.management.base import BaseCommand
 from dotenv import load_dotenv
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 from telegram.ext import CallbackContext, CommandHandler, Updater, CallbackQueryHandler, MessageHandler, Filters, ConversationHandler
 
 from bot.models import User, Cell, Order, Storage
 
+
+WELCOME = 0
+PROFILE = 1
+USER_REGISTATION = 2
+NEW_ORDER = 3
+GET_ADDRESS = 4
+COMMENT = 5
+MAKEORDER = 6
+
+BUTTON_APPROVE = "Прочитал и согласен"
 
 BUTTON_PERSONAL_DATA = "Посмотреть личные данные"
 BUTTON_MAIN_MENU = "Назад"
@@ -35,14 +45,27 @@ BUTTON_NO = "Нет"
 
 BUTTON_CONFIRM = "Подтвердить заказ"
 
+EDITABLE_DATA = None
+
 """
 ------------------------------------------------------------------------------
 Команды для работы с БД
 ------------------------------------------------------------------------------
 """
-def find_user(telegram_id: int):
-    return User.objects.get(telegram_id=telegram_id)
+def add_telegram_id(telegram_id: int):
+    user = User(telegram_id=telegram_id)
+    user.save()
 
+
+def find_user(telegram_id: int):
+    user = User.objects.filter(telegram_id=telegram_id)
+    if not user:
+        return False
+    return user[0]
+
+def get_user(telegram_id):
+    user = User.objects.get(telegram_id=telegram_id)
+    return user
 
 def find_cells(size):
     if size == "small":
@@ -55,13 +78,19 @@ def find_cells(size):
 
 def get_user_information(telegram_id):
     user = find_user(telegram_id=telegram_id)
-    print(user.name)
     user_information = f'''Имя: {user.name}
 Фамилия: {user.surname}
 Номер телефона: {user.phone}
 Адрес: {user.address}
 Email: {user.email}'''
     return user_information
+
+def get_order_information(order):
+    cell = order.cell
+    storage = cell.storage
+    message = f'''Дата создания заказа: {order.create_date}
+Ваши вещи находится в ячейке № {cell.number} на нашем складе по адресу: {storage.address}'''
+    return message
 
 
 def get_orders(telegram_id):
@@ -75,6 +104,22 @@ def get_number_orders(telegram_id):
     orders_count = user.orders.count()
     return orders_count
 
+
+def edit_user_data(text, user):
+    global EDITABLE_DATA
+    if EDITABLE_DATA == 'Имя':
+        user.name = text
+    elif EDITABLE_DATA == 'Фамилия':
+        user.surname = text
+    elif EDITABLE_DATA == 'Email':
+        user.email = text
+    elif EDITABLE_DATA == 'Адрес':
+        user.address = text
+    elif EDITABLE_DATA == 'Номер телефона':
+        user.phone = text
+    user.save()
+    EDITABLE_DATA = None
+    
 """
 ------------------------------------------------------------------------------
 Хэндлеры команд для бота
@@ -92,23 +137,6 @@ def start(update: Update, context: CallbackContext):
             reply_markup=reply_markup,
         )
     return WELCOME
-
-
-def account(update: Update, context: CallbackContext):
-    user_id = update.message.from_user.id
-    chat_id = update.message.chat_id
-    if find_user(user_id):
-        message = "Ваш личный кабинет"
-        reply_markup = get_account_keyboard(user_id)
-        
-        context.bot.send_message(
-            chat_id=chat_id,
-            text=message,
-            reply_markup=reply_markup,
-        )
-    else:
-        message = f"Мы вас не знаем, возможно вы не согласились на обработку ПД"
-        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
 def start_menu_handler(update: Update, context: CallbackContext):
@@ -133,7 +161,7 @@ def start_menu_handler(update: Update, context: CallbackContext):
             context.bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup,)
             return NEW_ORDER
         else:
-            message = "Сначала нужно Вас зарегистрировать, пожалуйста, поделитесь контактами"
+            message = "Для продолжения работы вы должны прочитать правила: <ссылка> хранения и согласиться на обработку пресональных данных: <ссылка>"
             reply_markup = get_contact()
             context.bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup,)
             return USER_REGISTATION
@@ -157,52 +185,60 @@ def start_menu_handler(update: Update, context: CallbackContext):
         return WELCOME
 
 
+def data_edit_message_handler(update: Update, context: CallbackContext):
+    global EDITABLE_DATA
+    if EDITABLE_DATA:
+        text = update.message.text
+        user_id = update.message.from_user.id
+        user = get_user(telegram_id=user_id)
+        edit_user_data(text, user)
+        reply_markup = get_data_edit_keyboard()
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Личные данные обновлены")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Что хотите изменить ещё?", reply_markup=reply_markup)
+
+
 
 def order_callback_handler(update: Update, context: CallbackContext):
     query = update.callback_query
     data = query.data
     chat_id = query.message.chat_id
-    user_id = user_id = update.callback_query.message.chat.id
+    user_id = update.callback_query.message.chat.id
+    measurer = False
     user = find_user(user_id)
-    measurer = True
-    if data == BUTTON_SMALL:
-        cells = find_cells(size="small")
-    elif data == BUTTON_MEDIUM:
-        cells = find_cells(size="medium")
-    elif data == BUTTON_LARGE:
-        cells = find_cells(size="large")
-    if data == BUTTON_SMALL or data == BUTTON_MEDIUM or data == BUTTON_LARGE:
+    if data in (BUTTON_SMALL, BUTTON_MEDIUM, BUTTON_LARGE):
         message = "Нужна ли Вам доставка?"
         reply_markup = get_yes_no_keyboard()
+        context.user_data["cell"] = data
         context.bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup)
     if data == BUTTON_YES and not user.address:
-        measurer = True
         message = "Пожалуйста, укажите Ваш адрес"
-        context.bot.send_message(chat_id=chat_id, text=message)
-        return GET_ADDRESS
-    elif data == BUTTON_NO or (data == BUTTON_YES and user.address):
-        measurer = False
-        message = "Добавьте комментарий к заказу, если это необходимо"
-        context.bot.send_message(chat_id=chat_id, text=message,)
-        return COMMENT
-    if data == BUTTON_CONFIRM:
-        new_order = Order(
-            user = user.id,
-            create_date=datetime.datetime.now(),
-            measurer=measurer,
-            comment = context.user_data.get("Comment", ""))
-        new_order.save()
-        message = "Ваш заказ создан, в ближайшее времся с Вами свяжется менеджер"
-        reply_markup = get_main_menu_keyboard(user)
+        measurer = True
         context.bot.send_message(
-            chat_id=chat_id,
+            chat_id=chat_id, 
             text=message,
-            reply_markup=reply_markup,
         )
-        return WELCOME
+    if data == BUTTON_NO or (data == BUTTON_YES and user.address):
+        message = "Добавьте комментарий к заказу, если это необходимо"
+        measurer = False
+        context.bot.send_message(
+            chat_id=chat_id, 
+            text=message,
+        )
+    if data == BUTTON_CONFIRM:
+        context.user_data["measurer"] = measurer
+        message = "Ваш заказ создан, в ближайшее времся с Вами свяжется менеджер"
+        order = Order(
+            user=user,
+            measurer=context.user_data["measurer"],
+            create_date = datetime.datetime.now(),
+            cell_id=2,
+            comment=context.user_data["comment"]
+        )
+        order.save()
 
 
 def keyboard_cabinet_callback_handler(update: Update, context: CallbackContext):
+    global EDITABLE_DATA
     query = update.callback_query
     data = query.data
     user_id = update.callback_query.message.chat.id
@@ -213,39 +249,48 @@ def keyboard_cabinet_callback_handler(update: Update, context: CallbackContext):
         
     elif data == BUTTON_EDIT_DATA:
         reply_markup = get_data_edit_keyboard()
-        message = "Что хотите изменить/добавить?"
+        message = "Что хотите изменить?"
         context.bot.send_message(chat_id=update.effective_chat.id, text=message, reply_markup=reply_markup)
     
     elif data == BUTTON_WIEW_ORDERS:
         reply_markup = get_account_keyboard(user_id)
         orders = get_orders(telegram_id=user_id)
         for order in orders:
-            
-# Нужно изменить после редактирования модели - Ячейка
-
-            message = f'''Дата создания: {order.create_date}
-Ваш комментарий: {order.comment}'''
+            message = get_order_information(order)
             context.bot.send_message(chat_id=update.effective_chat.id, text=message)
         message = "Ваш личный кабинет"
         context.bot.send_message(chat_id=update.effective_chat.id, text=message, reply_markup=reply_markup)
         
     elif data == BUTTON_MAIN_MENU:
-        pass
-    
+        message = "Добро пожаловать в сервис по хранению вещей!"
+        reply_markup = get_main_menu_keyboard(user_id)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message, reply_markup=reply_markup)
+        return WELCOME
+
     elif data == BUTTON_EDIT_NAME:
-        pass
+        EDITABLE_DATA = 'Имя'
+        message = "Введите Ваше имя:"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
         
     elif data == BUTTON_EDIT_SURNAME:
-        pass
+        EDITABLE_DATA = 'Фамилия'
+        message = "Введите Вашу Фамилию:"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
     
     elif data == BUTTON_EDIT_EMAIL:
-        pass
+        EDITABLE_DATA = 'Email'
+        message = "Введите Ваш email:"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
     
     elif data == BUTTON_EDIT_ADRESS:
-        pass
+        EDITABLE_DATA = 'Адрес'
+        message = "Введите Ваш адрес:"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
     elif data == BUTTON_EDIT_PHONE:
-        pass
+        EDITABLE_DATA = 'Номер телефона'
+        message = "Введите Ваш номер телефона:"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
     
     elif data == BUTTON_PERSONAL_ACCOUNT:
         message = "Ваш личный кабинет"
@@ -257,6 +302,7 @@ def keyboard_cabinet_callback_handler(update: Update, context: CallbackContext):
             reply_markup=reply_markup,
         )
 
+
 def contact_callback(update: Update, context: CallbackContext):
     contact = update.effective_message.contact
     user = User(
@@ -265,8 +311,7 @@ def contact_callback(update: Update, context: CallbackContext):
         surname = contact['last_name'],
         phone = contact['phone_number']
     )
-    user.save()
-    
+    user.save() 
     chat_id = update.message.chat_id
     message = "Выберите размер ячейки для хранения"
     reply_markup = get_cell_sizes()
@@ -274,25 +319,41 @@ def contact_callback(update: Update, context: CallbackContext):
     return NEW_ORDER
 
 
-def address_call_back(update: Update, context: CallbackContext):
+def address_callback(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id
-    user = find_user(telegram_id=user_id),
+    user = find_user(telegram_id=user_id)
     chat_id = update.message.chat_id
-    user.address = update.message
+    user.address = update.message.text
     user.save()
-    message = "Добавьте комментарий к заказу, если это необходимо"
-    context.bot.send_message(chat_id=chat_id, text=message,)
     return COMMENT
 
 
 def comment_callback(update: Update, context: CallbackContext):
-    context.user_data["Comment"] = update.message
-    message = "Заказ готов"
     chat_id = update.message.chat_id
+    message = "Принято"
     reply_markup = confirm_order()
     context.bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup,)
-    return WELCOME
+    return MAKEORDER
 
+
+def order_callback(update: Update, context: CallbackContext):
+    message = "Заказ сформирован"
+    chat_id = update.callback_query.message.chat_id
+    user_id = update.callback_query.message.chat.id
+    user = find_user(telegram_id=user_id)
+    order = Order(
+        user=user,
+        measurer=0,
+        create_date = datetime.datetime.now(),
+        cell_id=2,
+        comment="ок"
+        )
+    order.save()
+    reply_markup = get_account_keyboard(user_id)
+    context.bot.sendPhoto(chat_id=chat_id, photo=
+"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/250px-QR_code_for_mobile_English_Wikipedia.svg.png", caption="QR код ячейки")
+    context.bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup,)
+    return PROFILE
 
 def cancel(update: Update, context: CallbackContext):
     return ConversationHandler.END
@@ -303,7 +364,6 @@ def cancel(update: Update, context: CallbackContext):
 Клавиатуры
 ------------------------------------------------------------------------------
 """
-
 
 def get_account_keyboard(user_id):
     orders_count = get_number_orders(telegram_id=user_id)
@@ -348,10 +408,10 @@ def get_data_edit_keyboard():
 def get_contact():
     keyboard=[
         [
-            KeyboardButton("Поделиться контактом", request_contact=True),
+            KeyboardButton("Прочитал и согласен", request_contact=True),
         ],
     ]
-    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True,)
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
 
 
 def get_cell_sizes():
@@ -383,13 +443,6 @@ def confirm_order():
 
 
 
-WELCOME = 0
-PROFILE = 1
-USER_REGISTATION = 2
-NEW_ORDER = 3
-GET_ADDRESS = 4
-COMMENT = 5
-
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
@@ -401,14 +454,44 @@ class Command(BaseCommand):
         handler = ConversationHandler(
             entry_points=[CommandHandler('start', start)],
             states={
-                WELCOME: [CallbackQueryHandler(callback=start_menu_handler, pass_chat_data=True,)],
-                PROFILE: [CallbackQueryHandler(callback=keyboard_cabinet_callback_handler, pass_chat_data=True,)],
-                USER_REGISTATION: [MessageHandler(Filters.contact, contact_callback)],
-                NEW_ORDER: [CallbackQueryHandler(callback=order_callback_handler, pass_chat_data=True,)],
-                GET_ADDRESS: [MessageHandler(Filters.text, address_call_back)],
-                COMMENT: [MessageHandler(Filters.text, comment_callback)],
+                WELCOME: [CallbackQueryHandler(
+                    callback=start_menu_handler, 
+                    pass_chat_data=True,
+                )],
+                PROFILE: [CallbackQueryHandler(
+                    callback=keyboard_cabinet_callback_handler, 
+                    pass_chat_data=True,
+                )],
+                USER_REGISTATION: [MessageHandler(
+                    Filters.contact, 
+                    contact_callback
+                )],
+                NEW_ORDER: [
+                    CallbackQueryHandler(
+                        callback=order_callback_handler, 
+                        pass_chat_data=True,
+                    ),
+                    MessageHandler(
+                        Filters.text, 
+                        address_callback
+                    ),
+                ],
+                GET_ADDRESS: [MessageHandler(
+                    Filters.text, 
+                    address_callback
+                )],
+                COMMENT: [MessageHandler(
+                    Filters.text, 
+                    comment_callback,
+                    pass_chat_data=True,
+                )],
+                MAKEORDER: [CallbackQueryHandler(
+                    callback=order_callback, 
+                    pass_chat_data=True,
+                )],
+
             },
-            fallbacks=[CommandHandler('cancel', cancel)],
+            fallbacks=[CommandHandler('start', start)],
             )
         dispatcher.add_handler(handler)
 
